83d06df7db5dcf8d656bcc6b4894230d14502bf5
Lokki Steinsgate <lokki.steinsgate@ntymail.com>
2020-11-30 04:23:40 +0100

    table modification

--- script/script.js
+++ script/script.js
@@ -1,1 +1,26 @@
+let list = JSON("../listFilaments.json")
+let table = document.getElementsByTagName('tr')
 
+for (let i = 0; i <= 10; i++){
+	let tableLine = table[i].getElementsByTagName('th')
+	switch (tableLine.className){
+		case 'brand':
+			tableLine.innerHTML = list[i].brand
+			break
+		case 'price':
+			tableLine.innerHTML = list[i].price
+			break
+		case 'price bobine':
+			tableLine.innerHTML = list[i].bobine_price
+			break
+		case 'weight':
+			tableLine.innerHTML = list[i].bobine_weight
+			break
+		case 'material':
+			tableLine.innerHTML = list[i].material
+			break
+		case 'quality':
+			tableLine.innerHTML = list[i].quality
+			break
+	}
+}
