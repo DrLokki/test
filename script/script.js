8e92ba473e36d24b3f2e13097d61f6dabaea3831
Lokki Steinsgate <lokki.steinsgate@ntymail.com>
2020-12-07 04:23:47 +0100

    add choice function

--- script/script.js
+++ script/script.js
@@ -1,5 +1,15 @@
 let listF;
 let index = 0;
+let lenList = 0;
+
+const line = {
+    "brand": "",
+    "price": "",
+    "bobine_price": "",
+    "bobine_weight": "",
+    "material": "",
+    "quality": ""
+  }
 
 function updateTable(i){
 	let table = document.querySelectorAll("tbody>tr")
@@ -19,5 +29,34 @@ $(document).ready(function(){
 	$.getJSON("../listFilaments.json",function(file){
 		listF = file
 		updateTable(0)
+		lenList = listF.length%10
+		
+		if (lenList !== 0){
+			for (let i = 0; i <= lenList; i++){
+				listF.push(line)
+			}
+		}
 	})
 })
+
+function choice(choice){
+
+	switch(choice){
+		case 'first':
+			index = 0
+			break
+		case 'last':
+			index = listF.length-10
+			break
+		case 'next' :
+			if (index <= listF.length-20){
+				index += 10
+			}
+			break
+		case 'prev':
+			if(index >= 10){
+				index -= 10
+			}
+			break
+	}
+}
