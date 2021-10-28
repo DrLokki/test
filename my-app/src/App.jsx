9fbb264e5f62eb09b645b7827aad2f6fc792b805
DrLokki <lokki.steinsgate@ntymail.com>
2021-10-29 01:47:01 +0200

    add router

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from './view/Home/index.jsx';
+import FilamentsForm from './view/FilamentsForm/index.jsx';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/add" component={FilamentsForm} />
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 
