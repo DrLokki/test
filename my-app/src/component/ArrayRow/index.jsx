dba954c8fde9745c496a83b0c1853b5dbaaf7924
DrLokki <lokki.steinsgate@ntymail.com>
2021-10-29 01:48:17 +0200

    home page, table row

---
+++ my-app/src/component/ArrayRow/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import './styles.css';
+
+
+const ArrayRow = props => (
+	<React.Fragment>
+		<tr className="hover:bg-blue-100">
+			<td className="brand tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.brand}</td>
+			<td className="price tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.price}</td>
+			<td className="bobine_price tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.bobine_price}</td>
+			<td className="material tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.material}</td>
+			<td className="bobine_weight tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.bobine_weight}</td>
+			<td className="quality diagonal-fractions tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.quality}</td>
+	 </tr>
+	</React.Fragment>
+);
+
+export default ArrayRow;
