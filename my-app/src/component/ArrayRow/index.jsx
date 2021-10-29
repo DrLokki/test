16ee84a4c75bc3d2e1b139d591250a5cf135a678
DrLokki <lokki.steinsgate@ntymail.com>
2021-10-29 06:34:50 +0200

    visual correction

--- my-app/src/component/ArrayRow/index.jsx
+++ my-app/src/component/ArrayRow/index.jsx
@@ -5,13 +5,13 @@ import './styles.css';
 
 const ArrayRow = props => (
 	<React.Fragment>
-		<tr className="hover:bg-blue-100">
+		<tr className="hover:bg-cuisse">
 			<td className="brand tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.brand}</td>
 			<td className="price tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.price}</td>
 			<td className="bobine_price tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.bobine_price}</td>
 			<td className="material tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.material}</td>
 			<td className="bobine_weight tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.bobine_weight}</td>
-			<td className="quality diagonal-fractions tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.quality}</td>
+			<td className="quality diagonal-fractions tracking-tighter ipho:tracking-normal px-0 tbl:px-1 sm:px-3 text-center ">{props.quality}/10</td>
 	 </tr>
 	</React.Fragment>
 );
